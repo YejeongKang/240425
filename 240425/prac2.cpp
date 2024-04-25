e2f69db6de1ede7207d1f82d3cb4e5352180780b
YejeongKang <dpwjd1130@gmail.com>
2024-04-25 15:35:36 +0900

    age_check_fixed

--- 240425/prac2.cpp
+++ 240425/prac2.cpp
@@ -11,6 +11,8 @@ using namespace std;
 int main()
 {
 	int age;
+	int age_check = 0;
+
 	string student_info[ROW_SIZE][COL_SIZE] =
 	{
 		{ "이름", "나이", "국어","수학" },
@@ -20,12 +22,17 @@ int main()
 		{ "조나단", "20" , "82" , "29" }
 	};
 
-	cout << "점수를 찾는 학생의 나이를 입력하시오. : " << endl;
+	cout << "점수를 찾는 학생의 나이를 입력하시오. => " << endl;
 	cin >> age;
 
 	for (int i = 0; i < ROW_SIZE; i++) {
 		if (student_info[i][1] == to_string(age)) {
+			age_check++;
 			cout << "찾는 학생의 이름은 " << student_info[i][0] << ", 수학 점수는 " << student_info[i][3] << "점 입니다." << endl;
 		}
 	}
+	if (age_check == 0) {
+		cout << "해당 나이의 학생은 없숭..;";
+	}
+	return 0;
 }
