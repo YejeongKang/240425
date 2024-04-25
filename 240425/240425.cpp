66d0e4fbabf4b7dc536e7fdb6d79fd73c801d5af
YejeongKang <dpwjd1130@gmail.com>
2024-04-25 15:17:01 +0900

    Add project files.

---
+++ 240425/240425.cpp
@@ -0,0 +1,19 @@
+﻿
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+// 2차원 배열
+	int array[3][5] = { 0 };
+
+	int array[3][5] =
+	{
+		{ 1, 2 } , // row 0 = 1, 2, 0, 0, 0
+		{ 6, 7, 8 }, // row 1 = 6, 7, 8, 0, 0
+		{ 11, 12, 13, 14 } // row 2 = 11, 12, 13, 14 ;
+	};
+
+
+}
---
+++ 240425/prac2.cpp
@@ -0,0 +1,31 @@
+// 전처리기에서 작동됨.
+// 소스코드 이전에 실행되는 것들
+#include <iostream>
+#include <string>
+
+#define ROW_SIZE 5
+#define COL_SIZE 4
+
+using namespace std;
+
+int main()
+{
+	int age;
+	string student_info[ROW_SIZE][COL_SIZE] =
+	{
+		{ "이름", "나이", "국어","수학" },
+		{ "방지턱", "19" , "66" , "68" },
+		{ "정직한", "20" , "73" , "94" },
+		{ "김가루", "19" , "58" , "37" },
+		{ "조나단", "20" , "82" , "29" }
+	};
+
+	cout << "점수를 찾는 학생의 나이를 입력하시오. : " << endl;
+	cin >> age;
+
+	for (int i = 0; i < ROW_SIZE; i++) {
+		if (student_info[i][1] == to_string(age)) {
+			cout << "찾는 학생의 이름은 " << student_info[i][0] << ", 수학 점수는 " << student_info[i][3] << "점 입니다." << endl;
+		}
+	}
+}
