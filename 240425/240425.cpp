bb5548681f5a5c8a3277dc8083b38a5de8ee3f54
YejeongKang <dpwjd1130@gmail.com>
2024-04-25 19:30:32 +0900

    후엥

--- 240425/240425.cpp
+++ 240425/240425.cpp
@@ -1,11 +1,13 @@
 ﻿
 #include <iostream>
+#include <string>
 
 using namespace std;
 
+/*
 int main()
 {
-// 2차원 배열
+	// 2차원 배열
 	int array[3][5] = { 0 };
 
 	int array[3][5] =
@@ -15,5 +17,56 @@ int main()
 		{ 11, 12, 13, 14 } // row 2 = 11, 12, 13, 14 ;
 	};
 
+	// 형 변환 (정수->문자열)
+	int num = 0;
+	string snum = to_string(num);
+	
+
+	int main(){
+	// 포인터 : C++의 강점, 중요해서 대체 안됨
+	int n = 20;
+	int* p = &n;
+	cout << p << endl; // 주소값 : 000000B7230FF8A4
+	cout << *p << endl; // 값 : 20
 
 }
+*/
+	// 배열도 포인터로 작동
+	// 배열의 주소값은 순차적으로 적용되어 있음.
+	int main() {
+		int my_array[5] = { 5,4,3,2,1 };
+		int* p = my_array;
+
+		cout << "Value1: " << *p << endl; //5
+		p++;
+		cout << "Value2: " << *p << endl; //4
+		p++;
+		cout << "Value3: " << *p << endl; //3
+		return 0;
+	}
+	
+	// call by value : 주소값을 불러옴.
+	void changeValue(int x) {
+		x = 20;
+	}
+
+	int main() {
+		int x = 10;
+		cout << " x before function call : " << x << endl;
+		changeValue(x);
+		cout << " x after function call : " << x << endl;
+
+	}
+
+	// call by reference : 값을 불러옴. 
+	void changeValue(int *x) {
+		*x = 20;
+	}
+
+	int main() {
+		int x = 10;
+		cout << " x before function call : " << x << endl;
+		changeValue(&x);
+		cout << " x after function call : " << x << endl;
+
+	}
---
+++ 240425/prac3.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <string>
+
+#define Size 5
+
+using namespace std;
+
+// 배열을 뒤집어서 출력
+void reverse_print(string *arr) {
+	
+	for (int i = 0; i < Size; i++) {
+		cout << arr[Size - i - 1] << " ";
+	}
+}
+// 배열 그 잡채를 출력
+void origin_print(string *arr) {
+
+	for (int i = 0; i < Size; i++) {
+		cout << arr[i] << " ";
+	}
+}
+int main() {
+	
+	string korean[Size] = { "가", "나", "다", "라", "마" };
+	
+	cout << " korean before function call : ";
+	
+	origin_print(korean);
+
+	cout << endl << " korean after function call : ";
+	
+	reverse_print(korean);
+	
+}
